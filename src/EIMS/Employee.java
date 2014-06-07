c94e44ae8c468e1de3bad220b3b463fd0a9c6b05
cheeseduk <itsdavidxd@gmail.com>
2014-06-07 14:36:06 -0700

    Update Employee.java

--- src/EIMS/Employee.java
+++ src/EIMS/Employee.java
@@ -1,5 +1,32 @@
 package EIMS;
 
 public class Employee {
-  
+	public String firstname;
+	public String lastname;
+	public int empID;
+	public double salary;
+	public String department;
+	public int yearsofwork;
+	public String worklocation;
+
+	public Employee() {
+		firstname = "";
+		lastname = "";
+		empID = 0;
+		salary = 0.0;
+		department = "";
+		yearsofwork = 0;
+		worklocation = "";
+	}
+
+	public Employee(String fn, String ln, int ID, double sal, String dep,
+			int yow, String loc) {
+		firstname = fn;
+		lastname = ln;
+		empID = ID;
+		salary = sal;
+		department = dep;
+		yearsofwork = yow;
+		worklocation = loc;
+	}
 }
